2eb9777a73f7229afe1b90ccf890407a7694c5fe
mglbn <mglbn@github.com>
2026-07-05 22:21:26 +0200

    logs improved, still work needed

--- src/helper/logger.py
+++ src/helper/logger.py
@@ -1,11 +1,24 @@
-
 import logging
 from logging.handlers import TimedRotatingFileHandler
 
 
-logname = 'MonsterPi.log'
-log_handler = TimedRotatingFileHandler(logname, when='midnight', backupCount=30)
+logname = "MonsterPi.log"
+
+log_handler = TimedRotatingFileHandler(
+    logname,
+    when="midnight",
+    backupCount=30,
+    encoding="utf-8"
+)
 log_handler.suffix = "%Y%m%d"
-logger = logging.getLogger('DailyLogger')
+
+formatter = logging.Formatter(
+    fmt="%(asctime)s | %(levelname)-8s | %(name)s | %(module)s.%(funcName)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+log_handler.setFormatter(formatter)
+
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(log_handler)
