f776193247904414e88fadacf9565ce71ca307d6
mglbn <mglbn@github.com>
2026-07-06 17:57:39 +0200

    fixed services

--- scripts/push-to-opt.sh
+++ scripts/push-to-opt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 systemctl stop monsterpi.service
-rm -rf /opt/MonsterPiMonsterPi/src
+rm -rf /opt/MonsterPi/src
 cp -R "/home/$SUDO_USER/MonsterPi/src" /opt/MonsterPi/src
 chown -R kiosk:kiosk /opt/MonsterPi
 systemctl start monsterpi.service
